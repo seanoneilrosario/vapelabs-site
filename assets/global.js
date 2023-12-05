5030a83e3d678ec040196a302680e478ec579e0f
Sean Oneil <seanrosario119@gmail.com>
2023-12-05 13:11:28 +0800

    homepage sections added

--- assets/global.js
+++ assets/global.js
@@ -863,3 +863,50 @@ window.addEventListener("scroll", function () {
     image.style.transform = `translateY(-${es / 3}px)`;
   }
 })
+
+// slider
+$('.logo-slider_slider').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
+
+$('.feat-blog-post_slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
